5e08476976c428938a31e0edf694cadbe1894bd5
Arto Halonen <c8haar00@students.oamk.fi>
2021-01-10 23:37:58 +0200

    Last minute adjustments

--- app/serializers/user_serializer.rb
+++
@@ -1,8 +0,0 @@
-class UserSerializer
-  include FastJsonapi::ObjectSerializer
-  attributes :email, :created_at, :token
-
-  attribute :created_date do |user|
-    user.created_at && user.created_at.strftime('%d/%m/%Y')
-  end
-end
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,32 +4,3 @@
 # Examples:
 #
 
-projects = Project.create([
-
-    {
-        done:false,
-        date: '2020-08-23',
-        name: 'Oulun kaupunki',
-        customer: 'Suomen valtio'
-    },
-    {
-        done:true,
-        date: '2020-08-12',
-        name: 'Föödisköödi',
-        customer: 'FooBar'
-    }
-])
-
-tasks = Task.create([
-
-{
-    name: 'Paskan lapiointi',
-    hours: 5,
-    project: projects.first
-},
-{
-    name: 'Paskan lapiointi',
-    hours: 5,
-    project: projects.first
-}
-])
